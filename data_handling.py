804decfb44550073657e75f2df467ad9831886b4
Vivian VanRenterghem <vivianvanrenterghem@Vivians-MacBook-Air-58.local>
2024-12-11 16:55:55 -0500

    reading uv API

--- data_handling.py
+++ data_handling.py
@@ -1,6 +1,10 @@
+
 import urllib.request
 import json
 import pandas as pd
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
 
 # Define the API URL
 url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/michigan/2023-01-01/2023-12-31?elements=datetime%2Ctempmax%2Ctempmin&include=days&key=L5S34V39G8SNE7QB9SWETFPRD&contentType=json"
@@ -23,10 +27,6 @@ try:
     
     # Print every day and its high temperature
     print(weather_data.to_string(index=False))  # Print all rows without an index column
-
-    # Optionally save to a CSV file
-    weather_data.to_csv("michigan_weather_2023.csv", index=False)
-    print("Data saved to 'michigan_weather_2023.csv'")
     
 except urllib.error.HTTPError as e:
     error_info = e.read().decode()
@@ -35,3 +35,44 @@ except urllib.error.URLError as e:
     print("URL Error:", e.reason)
 except Exception as e:
     print("An error occurred:", e)
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": 44.3148,
+    "longitude": 85.6024,
+    "start_date": "2023-01-01",
+    "end_date": "2023-12-31",
+    "daily": "uv_index_max",
+    "temperature_unit": "fahrenheit",
+    "timezone": "America/New_York"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_uv_index_max = daily.Variables(0).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+    start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+    end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+    freq = pd.Timedelta(seconds = daily.Interval()),
+    inclusive = "left"
+)}
+daily_data["uv_index_max"] = daily_uv_index_max
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+print(daily_dataframe)
